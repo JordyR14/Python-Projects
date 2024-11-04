7b5bd453a64c7a2766529bec8accbcca34ede9a8
Jordy R <jordyruiz0326@gmail.com>
2024-11-03 22:04:02 -0500

    Add files via upload

---
+++ 1. Band Name Generator Project . B.py
@@ -0,0 +1,15 @@
+# Project 1 - Day 1 - 100 Days of Python Code
+
+# Greeting
+print("Band Name Generator Project..!!")
+print("Welcome to the Band Name Generator.")
+print("We will generate a suggestion of a band name after answering two questions.")
+
+# Ask the user for the city they grew up in and store it in the variable city
+city = input("What's the name of the city ou grew up in?: ")
+
+# Ask the user for the name of a pet and store it in the variable pet
+pet = input("What's your pet's name?: ")
+
+# Combine the name of their city and pet and show them their band name using concatenation
+print("Your band name could be " + city + " " + pet + "!")
